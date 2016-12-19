cd069222ba4ba6a7f22a2651dea754a2c0c44b16
Bram Jetten <mail@bramjetten.nl>
2016-12-19 13:43:59 +0100

    Password reset

---
+++ app/mailers/spina/customer_account_mailer.rb
@@ -0,0 +1,21 @@
+module Spina
+  class CustomerAccountMailer < ActionMailer::Base
+
+    def forgot_password(customer_account)
+      @customer_account = customer_account
+
+      mail(
+        to: @customer_account.email, 
+        from: current_account.email, 
+        subject: t('spina.shop.customer_accounts.forgot_password.title')
+      )
+    end
+
+    private
+
+      def current_account
+        Spina::Account.first # Replace with email setting Spina Shop
+      end
+
+  end
+end
--- app/models/spina/customer_account.rb
+++ app/models/spina/customer_account.rb
@@ -1,11 +1,12 @@
 module Spina
   class CustomerAccount < ApplicationRecord
     has_secure_password
+    has_secure_token :password_reset_token
     
     belongs_to :customer
 
     validates :email, presence: true, uniqueness: true
-    validates :password, length: {minimum: 6, maximum: 40}, on: [:create, :update]
+    # validates :password, length: {minimum: 6, maximum: 40}, on: [:create, :update]
 
     accepts_nested_attributes_for :customer
   end
