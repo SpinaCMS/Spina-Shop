f35c5e6200733d036f43eee3ba94ffc1464c6008
Bram Jetten <mail@bramjetten.nl>
2017-07-17 12:03:53 +0200

    Totals en btw berekenen

--- app/models/spina/shop/order/billing.rb
+++ app/models/spina/shop/order/billing.rb
@@ -5,24 +5,16 @@ module Spina::Shop
       order_items.inject(BigDecimal(0)) { |t, i| t + i.total }
     end
 
-    def order_total_excluding_tax
-      if prices_include_tax
-        order_items.inject(BigDecimal(0)) { |t, i| t + (i.total / i.tax_modifier).round(2) }
-      else
-        order_total
-      end
-    end
-
-    def order_total_including_tax
-      order_total_excluding_tax + tax_amount
-    end
-
     def delivery_price
       read_attribute(:delivery_price) || delivery_option.try(:price_for_order, self) || BigDecimal(0)
     end
 
     def delivery_tax_rate
       read_attribute(:delivery_tax_rate) || delivery_option.try(:tax_group).try(:tax_rate_for_order, self) || BigDecimal(0)
+    end
+
+    def delivery_tax_modifier
+      (delivery_tax_rate + BigDecimal(100)) / BigDecimal(100)
     end
 
     def gift_card_amount
@@ -39,11 +31,7 @@ module Spina::Shop
 
     # Total of the order
     def total
-      if prices_include_tax
-        order_total + delivery_price
-      else
-        order_total_excluding_tax + tax_amount + delivery_price
-      end
+      order_total + delivery_price + (prices_include_tax ? 0 : tax_amount)
     end
 
     def to_be_paid
@@ -68,20 +56,16 @@ module Spina::Shop
     end
 
     def tax_amount_by_rates
-      if prices_include_tax
-        rates = order_items.inject({}) do |h, item|
-          rate = h[item.tax_rate] ||= { tax_amount: BigDecimal(0), total: BigDecimal(0) }
+      rates = [order_items, OpenStruct.new(tax_rate: delivery_tax_rate, tax_modifier: delivery_tax_modifier, total: delivery_price)].flatten.inject({}) do |h, item|
+        rate = h[item.tax_rate] ||= { tax_amount: BigDecimal(0), total: BigDecimal(0) }
+        if prices_include_tax
           rate[:total] += (item.total / item.tax_modifier).round(2)
           rate[:tax_amount] += item.total - (item.total / item.tax_modifier).round(2)
-          h
-        end
-      else
-        rates = order_items.inject({}) do |h, item|
-          rate = h[item.tax_rate] ||= { tax_amount: BigDecimal(0), total: BigDecimal(0) }
+        else
           rate[:total] += item.total
           rate[:tax_amount] += (item.total * (BigDecimal(item.tax_rate) / BigDecimal(100))).round(2)
-          h
         end
+        h
       end
 
       rates.sort{|x, y| y[0] <=> x[0]}.to_h
