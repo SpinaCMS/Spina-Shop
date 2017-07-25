0ea17a0992083ab05806afc1910c647a930d4f3e
Bram Jetten <mail@bramjetten.nl>
2017-07-25 09:04:51 +0200

    Invoice

--- app/controllers/spina/shop/admin/invoices_controller.rb
+++ app/controllers/spina/shop/admin/invoices_controller.rb
@@ -4,7 +4,7 @@ module Spina::Shop
       def show
         @invoice = Invoice.find(params[:id])
         presenter = InvoicePresenter.new(@invoice, view_context)
-        pdf = InvoicePdf.new(presenter, locale: :en)
+        pdf = InvoicePdf.new(presenter)
         send_data pdf.render, filename: "inv_#{@invoice.invoice_number}.pdf", type: "application/pdf"
       end
     end
