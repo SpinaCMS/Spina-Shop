292e09bdf3954e5aa80b7ecf1b061c73c1e9b67a
Bram Jetten <mail@bramjetten.nl>
2017-07-25 14:26:25 +0200

    Admin helpers

--- lib/spina/shop/admin_helpers.rb
+++ lib/spina/shop/admin_helpers.rb
@@ -6,7 +6,6 @@ module Spina
         new_object = f.object.send(association).klass.new
         id = new_object.object_id
         fields = f.fields_for(association, new_object, child_index: id) do |builder|
-          build_structure_parts(f.object.page_part.name, new_object) if structure_item?(new_object)
           render("bundled_products_fields", f: builder)
         end
         link_to '#', class: "add_product_fields button button-link button-block", data: {id: id, fields: fields.gsub("\n", "")} do
@@ -18,7 +17,6 @@ module Spina
         new_object = f.object.send(association).klass.new
         id = new_object.object_id
         fields = f.fields_for(association, new_object, child_index: id) do |builder|
-          build_structure_parts(f.object.page_part.name, new_object) if structure_item?(new_object)
           render partial: "spina/shop/admin/settings/product_category_properties/property_options_fields", locals: {f: builder}
         end
         link_to '#', class: "add_property_option_fields button button-link button-block", data: {id: id, fields: fields.gsub("\n", "")} do
