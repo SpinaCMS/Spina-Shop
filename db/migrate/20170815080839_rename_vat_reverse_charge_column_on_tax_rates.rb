8a3abc6d1ba9f0847b768bfabcb16cc658428866
Bram Jetten <mail@bramjetten.nl>
2017-08-16 09:06:17 +0200

    locale

--- db/migrate/20170815080839_rename_vat_reverse_charge_column_on_tax_rates.rb
+++ db/migrate/20170815080839_rename_vat_reverse_charge_column_on_tax_rates.rb
@@ -1,5 +1,5 @@
 class RenameVatReverseChargeColumnOnTaxRates < ActiveRecord::Migration[5.1]
   def change
-    rename_column :spina_shop_tax_rates, :reverse_charge_vat, :vat_reverse_charge
+    remove_column :spina_shop_tax_rates, :reverse_charge_vat
   end
 end
