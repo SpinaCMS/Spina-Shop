137b61a0ec6e584f39dca7bb57a1cd8db540e1ae
Bram Jetten <mail@bramjetten.nl>
2017-09-12 10:10:03 +0200

    Hoger dan 0

--- app/controllers/spina/shop/admin/products/stock_forecasts_controller.rb
+++ app/controllers/spina/shop/admin/products/stock_forecasts_controller.rb
@@ -5,7 +5,7 @@ module Spina::Shop
 
         def show
           add_breadcrumb "Producten"
-          @products = Product.select('current_date + (floor((stock_level - 4) / trend) :: integer) as restock_date, *').active.where(stock_enabled: true, archived: false).includes(:product_images).where('sales_count > ?', 5).order('restock_date')
+          @products = Product.select('current_date + (floor((stock_level - 4) / trend) :: integer) as restock_date, *').where('trend > ?', 0).active.where(stock_enabled: true, archived: false).includes(:product_images).where('sales_count > ?', 5).order('restock_date')
           @products = @products.page(params[:page]).per(250)
         end
 
