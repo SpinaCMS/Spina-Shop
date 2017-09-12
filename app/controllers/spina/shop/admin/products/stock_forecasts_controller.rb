df68ab548a9a9aa04755ebf6157e6d0af38eae04
Bram Jetten <mail@bramjetten.nl>
2017-09-12 10:31:12 +0200

    Stock forecast

--- app/controllers/spina/shop/admin/products/stock_forecasts_controller.rb
+++ app/controllers/spina/shop/admin/products/stock_forecasts_controller.rb
@@ -6,7 +6,7 @@ module Spina::Shop
         def show
           add_breadcrumb "Producten"
           @products = Product.select('current_date + (floor((stock_level - 4) / trend) :: integer) as restock_date, *').where('trend > ?', 0).active.where(stock_enabled: true, archived: false).includes(:product_images).where('sales_count > ?', 5).order('restock_date')
-          @products = @products.page(params[:page]).per(250)
+          @products = @products.page(params[:page]).per(2500)
         end
 
       end
