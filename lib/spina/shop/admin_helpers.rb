ece0792d7be57d7ec2014147c8e0b7a1b229fc0c
Bram Jetten <mail@bramjetten.nl>
2017-10-10 10:13:39 +0200

    Adres aanpassen

--- lib/spina/shop/admin_helpers.rb
+++ lib/spina/shop/admin_helpers.rb
@@ -11,6 +11,17 @@ module Spina
         end
       end
 
+      def link_to_add_address(f, association, &block)
+        new_object = f.object.send(association).klass.new
+        id = new_object.object_id
+        fields = f.fields_for(association, new_object, child_index: id) do |builder|
+          render("address_fields", f: builder)
+        end
+        link_to '#', class: "add_address_fields button button-link", data: {id: id, fields: fields.gsub("\n", "")} do
+          block.yield
+        end
+      end
+
       def link_to_add_product_fields(f, association, &block)
         new_object = f.object.send(association).klass.new
         id = new_object.object_id
