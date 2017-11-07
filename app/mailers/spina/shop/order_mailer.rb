39292564d3b1efd2426bef296f38526cb6205dd1
Bram Jetten <mail@bramjetten.nl>
2017-11-07 11:52:56 +0100

    Woops

--- app/mailers/spina/shop/order_mailer.rb
+++ app/mailers/spina/shop/order_mailer.rb
@@ -7,7 +7,7 @@ module Spina::Shop
 
       mail(
         to: order.email, 
-        from: "#{current_acount.name} <#{current_account.email}>",
+        from: "#{current_account.name} <#{current_account.email}>",
         subject: t('spina.shop.emails.order_confirmation_title')
       )
     end
@@ -17,7 +17,7 @@ module Spina::Shop
 
       mail(
         to: order.email,
-        from: "#{current_acount.name} <#{current_account.email}>",
+        from: "#{current_account.name} <#{current_account.email}>",
         subject: t('spina.shop.emails.order_shipped_title')
       )
     end
