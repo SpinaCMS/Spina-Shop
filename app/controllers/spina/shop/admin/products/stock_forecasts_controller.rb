3196f9f5f259df120fbd8642852c738354c65c9d
Bram Jetten <mail@bramjetten.nl>
2017-11-21 17:11:29 +0100

    Stock forecast sneller maken

--- app/controllers/spina/shop/admin/products/stock_forecasts_controller.rb
+++ app/controllers/spina/shop/admin/products/stock_forecasts_controller.rb
@@ -5,8 +5,8 @@ module Spina::Shop
 
         def show
           add_breadcrumb "Producten"
-          @products = Product.select('current_date + (floor((stock_level - 4) / trend) :: integer) as restock_date, *').where('trend > ?', 0).active.where(stock_enabled: true, archived: false).includes(:product_images).where('sales_count > ?', 5).order('restock_date')
-          @products = @products.page(params[:page]).per(2500)
+          @products = Product.select('current_date + (floor((stock_level - 4) / trend) :: integer) as restock_date, (ceil(trend * 30) :: integer) as coming_30_days, *').where('trend > ?', 0).active.where(stock_enabled: true, archived: false).includes(:product_images, :translations).where('sales_count > ?', 5).order("#{params[:order] || 'restock_date'} #{params[:direction]}")
+          @products = @products.page(params[:page]).per(20)
         end
 
       end
