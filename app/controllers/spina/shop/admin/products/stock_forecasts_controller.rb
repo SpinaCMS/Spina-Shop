7c347da0e5ebf2ad5024f68c1a99319f4df1090a
Bram Jetten <mail@bramjetten.nl>
2018-03-14 11:55:56 +0100

    Superfast query

--- app/controllers/spina/shop/admin/products/stock_forecasts_controller.rb
+++ app/controllers/spina/shop/admin/products/stock_forecasts_controller.rb
@@ -5,8 +5,10 @@ module Spina::Shop
 
         def show
           add_breadcrumb "Producten"
-          @products = Product.select('current_date + (floor((stock_level - 4) / trend) :: integer) as restock_date, (ceil(trend * 30) :: integer) as coming_30_days, *').where('trend > ?', 0).active.where(stock_enabled: true, archived: false).includes(:product_images, :translations).where('sales_count > ?', 5).order("#{params[:order] || 'restock_date'} #{params[:direction]}")
-          @products = @products.page(params[:page]).per(20)
+
+          @products = Product.select('current_date + (floor((stock_level - 4) / trend) :: integer) as restock_date, (ceil(trend * 30) :: integer) as coming_30_days, SUM(CASE WHEN "spina_shop_stock_level_adjustments"."created_at" > current_date - interval \'30 days\' THEN "adjustment" ELSE 0 END) * -1 as past_30_days, SUM(CASE WHEN "spina_shop_stock_level_adjustments"."created_at" > current_date - interval \'90 days\' THEN "adjustment" ELSE 0 END) * -1 as past_90_days, SUM(CASE WHEN "spina_shop_stock_level_adjustments"."created_at" > current_date - interval \'42 days\' THEN "adjustment" ELSE 0 END) * -1 as optimal_stock, spina_shop_products.*').where('trend > ?', 0).active.where(stock_enabled: true, archived: false).joins(stock_level_adjustments: :order_item).includes(:product_images, :translations).where('adjustment < 0').where('sales_count > ?', 5).group('"spina_shop_products"."id"').order("#{params[:order] || 'restock_date'} #{params[:direction]}")
+
+          @products = @products.page(params[:page]).per(50)
         end
 
       end
