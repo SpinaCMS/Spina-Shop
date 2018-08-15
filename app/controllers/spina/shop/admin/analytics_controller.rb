71de8993173485124d9f64070fe0f125a5bf96a5
Bram Jetten <mail@bramjetten.nl>
2018-08-15 09:52:45 +0200

    Online offline

--- app/controllers/spina/shop/admin/analytics_controller.rb
+++ app/controllers/spina/shop/admin/analytics_controller.rb
@@ -20,15 +20,24 @@ module Spina::Shop
           "day"
         end
 
-        @invoices = Spina::Shop::Invoice.where(created_at: @from..@to).joins(:invoice_lines).group("date_trunc('#{@period}', spina_shop_invoices.created_at)").sum('CASE WHEN prices_include_tax THEN round(quantity * (unit_price / ((100 + tax_rate) / 100)), 2) ELSE quantity * unit_price END').sort_by(&:first)
+        case params[:online_offline]
+        when "online"
+          orders = Spina::Shop::Order.where(pos: false)
+        when "offline"
+          orders = Spina::Shop::Order.where(pos: true)
+        else
+          orders = Spina::Shop::Order.all
+        end
 
-        @cost_prices = Spina::Shop::Order.where(paid_at: @from..@to).joins(:order_items).group("date_trunc('#{@period}', spina_shop_orders.paid_at)").sum('-1 * unit_cost_price * quantity').sort_by(&:first)
+        @orders = orders.where(paid_at: @from..@to).joins(:order_items).group("date_trunc('#{@period}', spina_shop_orders.paid_at)").sum('CASE WHEN prices_include_tax THEN round(quantity * (unit_price / ((100 + tax_rate) / 100)), 2) - round(discount_amount / ((100 + tax_rate) / 100), 2) ELSE quantity * unit_price - discount_amount END').sort_by(&:first)
 
-        @profit = @invoices.map.with_index do |invoice, index|
+        @cost_prices = orders.where(paid_at: @from..@to).joins(:order_items).group("date_trunc('#{@period}', spina_shop_orders.paid_at)").sum('-1 * unit_cost_price * quantity').sort_by(&:first)
+
+        @profit = @orders.map.with_index do |invoice, index|
           invoice.last - -1 * @cost_prices[index].last
         end.to_json
 
-        @labels_json = @invoices.map do |date, total|
+        @labels_json = @orders.map do |date, total|
           if @period == "year"
             l(date, format: "%Y")
           elsif @period == "month"
@@ -38,7 +47,7 @@ module Spina::Shop
           end
         end.to_json
 
-        @data_json = @invoices.map(&:last).to_json
+        @data_json = @orders.map(&:last).to_json
         @cost_json = @cost_prices.map(&:last).to_json
       end
 
