89eebcefd347ec33ec1e1fbdda86915c1c55df22
Bram Jetten <mail@bramjetten.nl>
2018-08-22 11:03:35 +0200

    Analytics

--- app/controllers/spina/shop/admin/analytics_controller.rb
+++ app/controllers/spina/shop/admin/analytics_controller.rb
@@ -31,6 +31,10 @@ module Spina::Shop
 
         @orders = orders.where(paid_at: @from..@to).joins(:order_items).group("date_trunc('#{@period}', spina_shop_orders.paid_at)").sum('CASE WHEN prices_include_tax THEN round(quantity * (unit_price / ((100 + tax_rate) / 100)), 2) - round(discount_amount / ((100 + tax_rate) / 100), 2) ELSE quantity * unit_price - discount_amount END').sort_by(&:first)
 
+        @counts = orders.where(paid_at: @from..@to).joins(:order_items).group("date_trunc('#{@period}', spina_shop_orders.paid_at)").count.sort_by(&:first).map(&:last)
+
+        @discounts = orders.where(paid_at: @from..@to).joins(:order_items).group("date_trunc('#{@period}', spina_shop_orders.paid_at)").sum("CASE WHEN prices_include_tax THEN round(discount_amount / ((100 + tax_rate) / 100), 2) ELSE discount_amount END").sort_by(&:first).map(&:last)
+
         @cost_prices = orders.where(paid_at: @from..@to).joins(:order_items).group("date_trunc('#{@period}', spina_shop_orders.paid_at)").sum('-1 * unit_cost_price * quantity').sort_by(&:first)
 
         @profit = @orders.map.with_index do |invoice, index|
