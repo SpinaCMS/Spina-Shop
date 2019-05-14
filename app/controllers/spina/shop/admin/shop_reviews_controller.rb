7eda159bc01bb11966fc82c48274f92b91308f5a
Bram Jetten <mail@bramjetten.nl>
2019-05-14 11:24:11 +0200

    Edit shop reviews

--- app/controllers/spina/shop/admin/shop_reviews_controller.rb
+++ app/controllers/spina/shop/admin/shop_reviews_controller.rb
@@ -17,6 +17,28 @@ module Spina::Shop
         redirect_to spina.shop_admin_shop_reviews_path
       end
 
+      def edit
+        @shop_review = ShopReview.find(params[:id])
+        add_breadcrumb ShopReview.model_name.human(count: :other), spina.shop_admin_shop_reviews_path
+        add_breadcrumb @shop_review.author
+        render layout: "spina/admin/admin"
+      end
+
+      def update
+        @shop_review = ShopReview.find(params[:id])
+        if @shop_review.update_attributes(shop_review_params)
+          redirect_to spina.shop_admin_shop_reviews_path
+        else
+          render :edit
+        end
+      end
+
+      private
+
+        def shop_review_params
+          params.require(:shop_review).permit(:author, :review_pros, :review_cons, :score, :score_communication, :score_speed, :approved, :email)
+        end
+
     end
   end
 end
