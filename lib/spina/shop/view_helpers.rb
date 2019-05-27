76f90da2e3f8b1913cd87f535ff5d9f7f07f8462
Bram Jetten <mail@bramjetten.nl>
2019-05-27 10:42:44 +0200

    Badge with number of unapproved shop reviews

--- lib/spina/shop/view_helpers.rb
+++ lib/spina/shop/view_helpers.rb
@@ -6,6 +6,10 @@ module Spina
         @to_process_order_count ||= Order.in_state(:paid, :order_picking).count
       end
 
+      def unapproved_shop_reviews_count
+        @unapproved_shop_reviews_count ||= ShopReview.where(approved: false).count
+      end
+
     end
   end
 end
