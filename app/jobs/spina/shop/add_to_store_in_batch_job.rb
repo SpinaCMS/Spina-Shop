de3f0dc2216d0034b22b805f226eb6f52748be3c
Bram Jetten <mail@bramjetten.nl>
2019-06-24 12:12:30 +0200

    Store in batch processing

--- app/jobs/spina/shop/add_to_store_in_batch_job.rb
+++ app/jobs/spina/shop/add_to_store_in_batch_job.rb
@@ -3,7 +3,12 @@ module Spina::Shop
 
     def perform(product_ids, store_id)
       store = Store.find(store_id)
-      store.products << (Product.where(id: product_ids) + Product.where(parent_id: product_ids)).uniq
+
+      Product.where(id: product_ids).order("children_count DESC").each do |product|
+        product.stores = (product.stores + [store]).uniq
+        product.variant_overrides["stores"] = product.store_ids.sort != product.parent&.store_ids&.sort
+        product.save
+      end
 
       # Trigger save callback for parent products
       Product.where(id: product_ids).roots.where('children_count > 0').each(&:save)
