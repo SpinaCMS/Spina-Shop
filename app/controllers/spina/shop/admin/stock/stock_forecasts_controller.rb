953c579746a44cd0221471991fe5ddd848b1fcdf
Bram Jetten <mail@bramjetten.nl>
2019-09-09 12:19:46 +0200

    Also include products that arent sold yet in Stock Forecast

--- app/controllers/spina/shop/admin/stock/stock_forecasts_controller.rb
+++ app/controllers/spina/shop/admin/stock/stock_forecasts_controller.rb
@@ -7,7 +7,7 @@ module Spina::Shop
         def show
           add_breadcrumb "Voorraad"
 
-          @q = Product.stock_forecast.joins(:translations).where('adjustment <= 0').order("#{params[:order]} #{params[:direction]}").where(spina_shop_product_translations: {locale: I18n.locale}).group("spina_shop_products.id, spina_shop_product_translations.id").ransack(params[:q])
+          @q = Product.stock_forecast.joins(:translations).order("#{params[:order]} #{params[:direction]}").where(spina_shop_product_translations: {locale: I18n.locale}).group("spina_shop_products.id, spina_shop_product_translations.id").ransack(params[:q])
 
           @products = @q.result.page(params[:page]).per(50)
         end
