bf6f2a5afc60557754a62dcefda130ccad3ebd82
Bram Jetten <mail@bramjetten.nl>
2019-09-30 16:17:45 +0200

    Advanced filtering for stock forecast / recounts

--- app/controllers/spina/shop/admin/stock/stock_forecasts_controller.rb
+++ app/controllers/spina/shop/admin/stock/stock_forecasts_controller.rb
@@ -7,6 +7,14 @@ module Spina::Shop
         def show
           add_breadcrumb "Voorraad"
 
+          @unique_locations ||= Spina::Shop::Product.active.where.not(location: "").pluck('location').uniq.compact.sort
+          @locations = @unique_locations.map do |location|
+            regex = location.match(/\A(\d*)([a-zA-Z]*).*\z/)
+            regex.try(:[], 1).presence || regex.try(:[], 2).presence
+          end.uniq.compact.sort_by do |location|
+            location.length > 2 ? '1' : '0' + location
+          end
+
           @q = Product.stock_forecast.joins(:translations).order("#{params[:order]} #{params[:direction]}").where(spina_shop_product_translations: {locale: I18n.locale}).group("spina_shop_products.id, spina_shop_product_translations.id").ransack(params[:q])
 
           @products = @q.result.page(params[:page]).per(50)
