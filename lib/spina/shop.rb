c8f72809410db66c87695b341381360d1c77b8b7
Bram Jetten <mail@bramjetten.nl>
2019-11-12 13:41:50 +0100

    Add support for invoices date due

--- lib/spina/shop.rb
+++ lib/spina/shop.rb
@@ -5,11 +5,14 @@ module Spina
   module Shop
     include ActiveSupport::Configurable
 
-    config_accessor :default_tax_rate, :default_tax_code, :default_sales_category_code, :payment_methods_for_manual_orders
+    config_accessor :default_tax_rate, :default_tax_code, :default_sales_category_code, :payment_methods_for_manual_orders, :invoice_payment_term
 
     # Default tax settings
     self.default_tax_rate = BigDecimal(0)
     self.default_tax_code = "0"
+
+    # Period to calculate the due date on an invoice
+    self.invoice_payment_term = 30.days
 
     # Manual payment methods are payment methods
     # that can be set when creating an order manually through Spina
