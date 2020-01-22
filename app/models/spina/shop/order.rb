4f2b3f4a76adb351e2dbbfa56649a490548928aa
Bram Jetten <mail@bramjetten.nl>
2020-01-22 13:13:22 +0100

    Validate order max

--- app/models/spina/shop/order.rb
+++ app/models/spina/shop/order.rb
@@ -65,6 +65,7 @@ module Spina::Shop
     # Validate Stock
     validate :items_must_be_in_stock, if: -> { validate_stock }
     validate :must_have_at_least_one_item, if: -> { validate_stock }
+    validate :items_must_be_below_limit, if: -> { validate_stock }
     before_validation :validate_stock_for_order_items, if: -> { validate_stock }
 
     accepts_nested_attributes_for :order_items
@@ -239,6 +240,10 @@ module Spina::Shop
 
       def items_must_be_in_stock
         errors.add(:base, :stock_level_not_sufficient) unless order_items.all?(&:in_stock?)
+      end
+
+      def items_must_be_below_limit
+        errors.add(:base, :item_limit_exceeded) unless order_items.all?(&:below_limit?)
       end
 
       def validate_stock_for_order_items
