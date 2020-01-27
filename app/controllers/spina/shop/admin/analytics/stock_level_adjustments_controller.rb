b8c434cd0043ada6230638511061cd79745aa112
Bram Jetten <mail@bramjetten.nl>
2020-01-27 16:38:27 +0100

    Detailed view

--- app/controllers/spina/shop/admin/analytics/stock_level_adjustments_controller.rb
+++ app/controllers/spina/shop/admin/analytics/stock_level_adjustments_controller.rb
@@ -6,13 +6,17 @@ module Spina::Shop
 
         before_action :set_breadcrumbs
 
+        def show
+          @stock_level_adjustments = StockLevelAdjustment.where(category: params[:category]).where("EXTRACT(WEEK FROM
+created_at)::int = ?", params[:id]).order(:created_at)
+        end
+
         def index
           params[:start_date] ||= l(Date.today.beginning_of_month.beginning_of_week, format: "%d-%m-%Y")
           params[:end_date] ||= l(Date.today.end_of_month.end_of_week, format: "%d-%m-%Y")
 
           @start_date = Date.parse(params[:start_date])
           @end_date = Date.parse(params[:end_date])
-          @interval = params[:interval] || "week"
 
           @stock_level_adjustments = StockLevelAdjustment.where.not(category: nil).where("created_at::date >= ? AND created_at::date <= ?", @start_date, @end_date).select("EXTRACT(WEEK FROM
 created_at)::int as weekly, category, SUM(adjustment) as total").group("weekly, category")
