64e266c8c7f5d6b1d32acc8ef6f5ddf71064b4da
Bram Jetten <mail@bramjetten.nl>
2020-03-09 15:54:00 +0100

    Edit stock multiple

--- app/assets/javascripts/spina/mobile/application.js
+++ app/assets/javascripts/spina/mobile/application.js
@@ -1,3 +1,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+
+$(document).on("click", "#edit_stock", function() {
+
+  $(this).next().show();
+  $(this).remove();
+
+});
--- app/controllers/spina/shop/stock_management/recounts_controller.rb
+++ app/controllers/spina/shop/stock_management/recounts_controller.rb
@@ -8,39 +8,54 @@ module Spina::Shop
       end
 
       def create
-        # Adjust for reserved stock
-        recount_stock_level = recount_params[:stock_level].to_i - @reserved
+        if recount_params[:stock_levels].any?{|s| s[:stock_level].present?}
+          original_stock_level = @product.stock_level.to_i
+          recount_stock_level = recount_params[:stock_levels].inject(0){|t,i| t = t + i[:stock_level].to_i} + @reserved
+          recount_difference = recount_stock_level - original_stock_level
 
-        original_stock_level = @product.stock_level.to_i
-        recount_difference = recount_stock_level - original_stock_level.to_i
-        something_changed = (recount_difference != 0) || (recount_params[:expiration_year].to_i != @product.expiration_year.to_i) || (recount_params[:expiration_month].to_i != @product.expiration_month.to_i)
-        difference_params = {difference: recount_difference, actor: current_spina_user.try(:name) || "onbekend"}
-
-        if something_changed
-          Product.transaction do
-            # Reset stock first
+          @product.transaction do
+            # Reset stock to 0
             ChangeStockLevel.new(@product, {
               adjustment: @product.stock_level * -1,
-              description: "Stock Management Reset",
+              description: "Recount - Reset",
               actor: current_spina_user.try(:name)
             }).save
 
-            # Create new stock entry
-            ChangeStockLevel.new(@product, {
-              adjustment: recount_stock_level,
-              description: "Stock Management Recount (#{recount_difference} difference)",
-              expiration_year: recount_params[:expiration_year],
-              expiration_month: recount_params[:expiration_month],
-              actor: current_spina_user.try(:name)
-            }).save
+            # Add new stock for each in recount_params[:stock_levels]
+            stock_levels = recount_params[:stock_levels].sort_by do |stock_level|
+              if stock_level[:expiration_month].present? && stock_level[:expiration_year].present?
+                Date.parse("01-#{stock_level[:expiration_month]}-#{stock_level[:expiration_year]}")
+              end
+            end.each do |stock_level|
+              next if stock_level[:stock_level].to_i.zero? # Skip any zero stock levels
+              ChangeStockLevel.new(@product, {
+                adjustment: stock_level[:stock_level],
+                description: "Recount",
+                expiration_year: stock_level[:expiration_year],
+                expiration_month: stock_level[:expiration_month],
+                actor: current_spina_user&.name
+              }).save
+            end
 
-            # Save the actual recount
-            @product.recounts.create(difference_params)
+            # Re-add reserved stock
+            if @reserved != 0
+              ChangeStockLevel.new(@product, {
+                adjustment: @reserved,
+                description: "Recount - Reserved",
+                actor: current_spina_user&.name
+              }).save
+            end
+
+            # Save as recount
+            @product.recounts.create({
+              difference: recount_difference, 
+              actor: current_spina_user&.name
+            })
           end
         else
-          # Save an empty recount
-          @product.recounts.create(difference_params)
+          @product.recounts.create({difference: 0, actor: current_spina_user&.name})
         end
+
         redirect_to spina.new_shop_stock_management_product_recount_path(@next_product)
       end
 
@@ -48,7 +63,7 @@ module Spina::Shop
 
         def set_product
           @product = Product.find_by(id: params[:product_id])
-          @reserved = -1 * @product.stock_level_adjustments.joins(order_item: :order).where(spina_shop_orders: {id: Spina::Shop::Order.to_process.ids}).sum(:adjustment)
+          @reserved = @product.stock_level_adjustments.joins(order_item: :order).where(spina_shop_orders: {id: Spina::Shop::Order.to_process.ids}).sum(:adjustment)
         end
 
         def set_previous_product
@@ -68,7 +83,7 @@ module Spina::Shop
         end
 
         def recount_params
-          params.require(:product).permit(:stock_level, :expiration_year, :expiration_month)
+          params.require(:product).permit(stock_levels: [:stock_level, :expiration_month, :expiration_year])
         end
 
     end
