9395532bff02c16080ab87b0ff268c65ad64fca3
Bram Jetten <mail@bramjetten.nl>
2020-03-10 13:35:22 +0100

    Highlight input fields when receiving stock

--- app/assets/javascripts/spina/mobile/application.js
+++ app/assets/javascripts/spina/mobile/application.js
@@ -1,6 +1,12 @@
 //= require jquery
 //= require jquery_ujs
+
 //= require turbolinks
+
+// Stimulus
+
+//= require spina/stimulus.umd
+//= require_tree ./controllers
 
 $(document).on("click", "#edit_stock", function() {
 
---
+++ app/assets/javascripts/spina/mobile/controllers/stock_order_controller.js
@@ -0,0 +1,32 @@
+(() => {
+  const application = Stimulus.Application.start()
+
+  application.register("stock-order", class extends Stimulus.Controller {
+    static get targets() {
+      return ["receivedField"]
+    }
+
+    connect() {
+      this.validateReceivedField()
+    }
+
+    validateReceivedField() {
+      if (this.receivedInput == this.shouldInput) {
+        this.receivedFieldTarget.classList.remove("warning")
+        this.receivedFieldTarget.classList.add("success")
+      } else {
+        this.receivedFieldTarget.classList.remove("success")
+        this.receivedFieldTarget.classList.add("warning")
+      }
+    }
+
+    get shouldInput() {
+      return parseInt(this.element.dataset.shouldReceive)
+    }
+
+    get receivedInput() {
+      return parseInt(this.receivedFieldTarget.value)
+    }
+
+  })
+})()
