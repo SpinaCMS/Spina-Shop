b57a0851d2e0bbc7998a2df8e2e747f5bdde2e3b
Bram Jetten <mail@bramjetten.nl>
2020-06-03 10:46:46 +0200

    Migration version

--- db/migrate/20200114140811_add_category_to_spina_shop_stock_level_adjustments.rb
+++ db/migrate/20200114140811_add_category_to_spina_shop_stock_level_adjustments.rb
@@ -1,4 +1,4 @@
-class AddCategoryToSpinaShopStockLevelAdjustments < ActiveRecord::Migration[6.0]
+class AddCategoryToSpinaShopStockLevelAdjustments < ActiveRecord::Migration[5.2]
   def change
     add_column :spina_shop_stock_level_adjustments, :category, :string
   end
