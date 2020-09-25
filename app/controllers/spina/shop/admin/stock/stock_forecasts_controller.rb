777448d64e55754f0b2c5e00ab674b438466f8eb
Bram Jetten <mail@bramjetten.nl>
2020-09-25 18:14:23 +0200

    Download CSV table

--- app/controllers/spina/shop/admin/stock/stock_forecasts_controller.rb
+++ app/controllers/spina/shop/admin/stock/stock_forecasts_controller.rb
@@ -16,8 +16,39 @@ module Spina::Shop
           end
 
           @q = Product.stock_forecast.joins(:translations).order("#{params[:order]} #{params[:direction]}").where(spina_shop_product_translations: {locale: I18n.locale}).group("spina_shop_products.id, spina_shop_product_translations.id").ransack(params[:q])
-
           @products = @q.result.page(params[:page]).per(50)
+
+          respond_to do |format|
+            format.js
+            format.html
+            format.csv do
+              data = CSV.generate do |csv|
+                csv << %w(ID Product Locatie Lopen30 Lopen90 Lopen365 Verkoop30 Verkoop90 Verkoop365 Voorraad Optimale\ voorraad Voorraadverschil Doorlooptijd Herinneren Verkoopprijs Kostprijs Voorraadwaarde)
+                @products.each.each do |product|
+                  csv << [product.id, 
+                    product.full_name, 
+                    product.location, 
+                    product.order_picking_30_days, 
+                    product.order_picking_90_days, 
+                    product.order_picking_365_days,
+                    product.past_30_days,
+                    product.past_90_days,
+                    product.past_365_days,
+                    product.stock_level,
+                    product.optimal_stock,
+                    product.stock_difference,
+                    product.lead_time.round,
+                    product.in_stock_reminders_count,
+                    view_context.number_to_currency(product.base_price),
+                    view_context.number_to_currency(product.cost_price),
+                    view_context.number_to_currency(product.stock_value)
+                  ]
+                end
+              end
+              
+              send_data data, filename: "voorraad.csv", disposition: "attachment"
+            end
+          end
         end
 
       end
