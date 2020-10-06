c2bb8d92f01c430f630dad255e27a2fc03d1e197
Bram Jetten <mail@bramjetten.nl>
2020-10-06 13:36:39 +0200

    Batch form modal

--- app/assets/javascripts/spina/shop/admin/controllers/batch_form_controller.js
+++ app/assets/javascripts/spina/shop/admin/controllers/batch_form_controller.js
@@ -3,15 +3,9 @@
 
   application.register("batch-form", class extends Stimulus.Controller {
 
-    connect() {
-      
-    }
-
     submit(event) {
-      event.preventDefault()
       let button = event.currentTarget
       this.element.action = button.dataset.url
-      $(this.element).trigger("submit.rails")
     }
 
   })
