5e60463c063de9be524db9537d595097f47b7d7f
Bram Jetten <mail@bramjetten.nl>
2021-02-26 09:28:35 +0100

    Order form controller

--- app/assets/javascripts/spina/shop/admin/controllers/order_form_controller.js
+++ app/assets/javascripts/spina/shop/admin/controllers/order_form_controller.js
@@ -4,7 +4,7 @@
   application.register("order-form", class extends Stimulus.Controller {
 
     static get targets() {
-      return ["customerId", "firstName", "lastName", "email", "phone", "company", "billingStreet1", "billingStreet2", "billingHouseNumber", "billingHouseNumberAddition", "billingPostalCode", "billingCity", "billingCountry"]
+      return ["customerId", "firstName", "lastName", "email", "phone", "company", "billingStreet1", "billingStreet2", "billingHouseNumber", "billingHouseNumberAddition", "billingPostalCode", "billingCity", "billingCountry", "billingName"]
     }
 
     copy_details() {
@@ -14,20 +14,21 @@
         json = JSON.parse(json)
 
         // Details
-        this.firstNameTarget.value = json.first_name
-        this.lastNameTarget.value = json.last_name
-        this.emailTarget.value = json.email
-        this.phoneTarget.value = json.phone
-        this.companyTarget.value = json.company
+        
+        if (this.hasFirstNameTarget) this.firstNameTarget.value = json.first_name
+        if (this.hasLastNameTarget) this.lastNameTarget.value = json.last_name
+        if (this.hasEmailTarget) this.emailTarget.value = json.email
+        if (this.hasPhoneTarget) this.phoneTarget.value = json.phone
+        if (this.hasCompanyTarget) this.companyTarget.value = json.company
 
         // Billing address
-        this.billingStreet1Target.value = json.billing_street_1
-        this.billingStreet2Target.value = json.billing_street_2
-        this.billingHouseNumberTarget.value = json.billing_house_number
-        this.billingHouseNumberAdditionTarget.value = json.billing_house_number_addition
-        this.billingPostalCodeTarget.value = json.billing_postal_code
-        this.billingCityTarget.value = json.billing_city
-        this.billingCountryTarget.value = json.billing_country
+        if (this.hasBillingStreet1Target) this.billingStreet1Target.value = json.billing_street_1
+        if (this.hasBillingStreet2Target) this.billingStreet2Target.value = json.billing_street_2
+        if (this.hasBillingHouseNumberTarget) this.billingHouseNumberTarget.value = json.billing_house_number
+        if (this.hasBillingHouseNumberAdditionTarget) this.billingHouseNumberAdditionTarget.value = json.billing_house_number_addition
+        if (this.hasBillingPostalCodeTarget) this.billingPostalCodeTarget.value = json.billing_postal_code
+        if (this.hasBillingCityTarget) this.billingCityTarget.value = json.billing_city
+        if (this.hasBillingCountryTarget) this.billingCountryTarget.value = json.billing_country
       }.bind(this))
     }
 
