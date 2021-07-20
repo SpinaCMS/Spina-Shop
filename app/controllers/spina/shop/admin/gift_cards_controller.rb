a91c57150e075ba1d64616c3bb8abbc60c8e1694
Bram Jetten <mail@bramjetten.nl>
2021-07-20 10:46:35 +0200

    Edit gift card expires_at

--- app/controllers/spina/shop/admin/gift_cards_controller.rb
+++ app/controllers/spina/shop/admin/gift_cards_controller.rb
@@ -1,7 +1,7 @@
 module Spina::Shop
   module Admin
     class GiftCardsController < AdminController
-      before_action :set_gift_card, only: [:show, :destroy]
+      before_action :set_gift_card, only: [:show, :edit, :update, :destroy]
       before_action :set_breadcrumbs
 
       def index
@@ -36,6 +36,19 @@ module Spina::Shop
           render :new
         end
       end
+      
+      def edit
+        add_breadcrumb @gift_card.readable_code
+      end
+      
+      def update
+        if @gift_card.update(gift_card_expiration_params)
+          redirect_to spina.shop_admin_gift_card_path(@gift_card)
+        else
+          add_breadcrumb @gift_card.readable_code
+          render :edit
+        end
+      end
 
       def destroy
         @gift_card.destroy
@@ -54,6 +67,10 @@ module Spina::Shop
 
         def set_breadcrumbs
           add_breadcrumb GiftCard.model_name.human(count: 2), spina.shop_admin_gift_cards_path
+        end
+        
+        def gift_card_expiration_params
+          params.require(:gift_card).permit(:expires_at)
         end
 
         def gift_card_params
