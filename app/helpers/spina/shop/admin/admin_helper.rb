fb767b52616f15bf249b780ce87a47f06118da75
Bram Jetten <mail@bramjetten.nl>
2022-01-21 10:09:22 +0100

    Regular datestamp

--- app/helpers/spina/shop/admin/admin_helper.rb
+++ app/helpers/spina/shop/admin/admin_helper.rb
@@ -8,24 +8,6 @@ module Spina::Shop
         ""
       end
       
-      def format_datetime(datetime)
-        formatted = if datetime.to_date == Date.today
-          l(datetime, format: "Vandaag, %H:%M")
-        elsif datetime.to_date == Date.yesterday
-          l(datetime, format: "Gisteren, %H:%M")
-        elsif datetime.to_date == Date.tomorrow
-          l(datetime, format: "Morgen, %H:%M")
-        else
-          l(datetime, format: "%e %b. %y, %H:%M")
-        end
-        
-        if datetime.is_a? DateTime
-          formatted << l(datetime, format: ", %H:%M")
-        end
-        
-        formatted
-      end
-      
     end
   end
 end
