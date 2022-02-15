97656cba12349b87371dba866903534e6beec8c4
Bram Jetten <mail@bramjetten.nl>
2022-02-15 17:05:43 +0100

    Layout fix Spina

--- app/controllers/spina/shop/admin/customers_controller.rb
+++ app/controllers/spina/shop/admin/customers_controller.rb
@@ -29,7 +29,7 @@ module Spina::Shop
       def new
         @customer = Customer.new
         add_breadcrumb t('spina.shop.customers.new')
-        render layout: 'spina/admin/admin'
+        render layout: 'spina/shop/admin/admin'
       end
 
       def create
@@ -71,7 +71,7 @@ module Spina::Shop
         @customer = Customer.find(params[:id])
         add_breadcrumb @customer.name, spina.shop_admin_customer_path(@customer)
         add_breadcrumb t('spina.edit')
-        render layout: 'spina/admin/admin'
+        render layout: 'spina/shop/admin/admin'
       end
 
       def update
