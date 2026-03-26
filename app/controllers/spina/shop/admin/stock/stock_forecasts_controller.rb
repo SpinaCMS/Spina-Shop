48b3ab32de85bca6b5a0089e365a458d17006831
Martijn van der Heiden <martijn@10kb.nl>
2026-03-26 12:27:25 +0100

    Make sure the ransack controller can handle the form overwritten in smokesmarter StockForecastsController

--- app/controllers/spina/shop/admin/stock/stock_forecasts_controller.rb
+++ app/controllers/spina/shop/admin/stock/stock_forecasts_controller.rb
@@ -6,6 +6,13 @@ module Spina::Shop
       class StockForecastsController < AdminController
         layout 'spina/shop/admin/stock'
 
+        FORECAST_ORDER_COLUMNS = %w[
+          location order_picking_30_days order_picking_90_days past_90_days past_365_days
+          statistics_weekly_sales statistics_safety_stock stock_level stock_difference
+          lead_time recount_created_at base_price cost_price
+        ].freeze
+        FORECAST_ORDER_DIRECTIONS = %w[asc desc].freeze
+
         def show
           add_breadcrumb 'Voorraad'
 
@@ -17,22 +24,35 @@ module Spina::Shop
             location.length > 2 ? '1' : '0' + location
           end
 
-          products = Product.joins('LEFT JOIN spina_shop_suppliers ON spina_shop_products.supplier_id = spina_shop_suppliers.id').where(stock_enabled: true, archived: false).joins(:translations).order("#{params[:order]} #{params[:direction]}").where(spina_shop_product_translations: {locale: I18n.locale}).group('spina_shop_products.id, spina_shop_product_translations.id')
+          order_column = FORECAST_ORDER_COLUMNS.include?(params[:order]) ? params[:order] : 'location'
+          order_direction = FORECAST_ORDER_DIRECTIONS.include?(params[:direction]) ? params[:direction] : 'asc'
 
-          if params[:order] == 'statistics_safety_stock'
+          products = Product.joins('LEFT JOIN spina_shop_suppliers ON spina_shop_products.supplier_id = spina_shop_suppliers.id').where(stock_enabled: true, archived: false).joins(:translations).order(Product.arel_table[order_column].send(order_direction == 'desc' ? :desc : :asc)).where(spina_shop_product_translations: {locale: I18n.locale}).group('spina_shop_products.id, spina_shop_product_translations.id')
+
+          if order_column == 'statistics_safety_stock'
             products = products.reorder(Arel.sql('CASE WHEN stock_level - statistics_safety_stock < 0 THEN stock_level - statistics_safety_stock ELSE 1 END, CASE WHEN stock_level - statistics_reorder_point < 0 THEN stock_level - statistics_reorder_point ELSE 1 END'))
             products = products.where(available_at_supplier: true).where('statistics_reorder_point > 0')
           end
 
           @q = products.where('sku ILIKE :search OR location ILIKE :search OR translations_name ILIKE :search OR translations_variant_name ILIKE :search', search: "%#{params[:search]}%")
           @q = @q.where(product_category_id: params[:product_category_id_in]) if params[:product_category_id_in].present?
-          @q = @q.where(location: params[:location_start_any]) if params[:location_start_any].present?
+          if params[:location_start_any].present?
+            prefixes = Array(params[:location_start_any]).map(&:presence).compact
+            if prefixes.any?
+              template = prefixes.map { 'spina_shop_products.location ILIKE ?' }.join(' OR ')
+              values = prefixes.map { |loc| "#{ActiveRecord::Base.sanitize_sql_like(loc.to_s)}%" }
+              @q = @q.where(template, *values)
+            end
+          end
           @q = @q.where('recounts_created_at >= :recounts_created_at_gteq', recounts_created_at_gteq: params[:recounts_created_at_gteq]) if params[:recounts_created_at_gteq].present?
           @q = @q.where(supplier_id: params[:supplier_id_in]) if params[:supplier_id_in].present?
           @q = @q.where(available_at_supplier: params[:available_at_supplier_eq]) if params[:available_at_supplier_eq].present?
           @q = @q.where(statistics_weekly_sales: params[:statistics_weekly_sales_eq]) if params[:statistics_weekly_sales_eq].present?
 
-          @products = @q.distinct.limit(50).offset((params[:page].to_i || 1) * 50 - 50)
+          page = (params[:page].presence || 1).to_i
+          page = 1 if page < 1
+
+          @products = @q.distinct.limit(50).offset((page - 1) * 50)
 
           respond_to do |format|
             format.js
@@ -43,7 +63,7 @@ module Spina::Shop
 
                 csv << %w(ID Product EAN Locatie Formaat Inhoud Lopen30 Lopen365 Verkoop30 Verkoop365 Wekelijkse\ verkoop Voorraad Max\ Voorraad Veiligheidsvoorraad Bestelpunt EOQ Inkooporders\ per\ jaar Leverancier Categorie Verpakkingseenheid Laatste\ hertelling THT Uitlopend Verkoopprijs Kostprijs XYZ Kosten\ per\ leveringsorder)
 
-                @products = @q.distinct.limit(5000).offset((params[:page].to_i || 1) * 5000 - 5000)
+                @products = @q.distinct.limit(5000).offset((page - 1) * 5000)
 
                 @products.each.each do |product|
                   recount = product.recounts.order(created_at: :desc).first
